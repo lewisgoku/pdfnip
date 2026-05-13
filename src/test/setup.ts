b0f0c98bbda998d4c6bdfdd8adfcc1d81642816e
Lewis Okuguni <lewisokuguni@gmail.com>
2026-05-13 10:53:29 -0700

    test: stub URL.createObjectURL and revokeObjectURL in test setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom'
+import { vi } from 'vitest'
+
+Object.defineProperty(URL, 'createObjectURL', {
+  writable: true,
+  value: vi.fn(() => 'blob:mock-url'),
+})
+Object.defineProperty(URL, 'revokeObjectURL', {
+  writable: true,
+  value: vi.fn(),
+})
